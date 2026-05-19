eb194a2fbee9e74ec1d3e2b8ed54c90cf1f23088
saim-alam1 <allbehassansaim@gmail.com>
2026-05-20 01:23:50 +0600

    Customized Footer Section

--- src/components/Pages/Footer/Footer.jsx
+++ src/components/Pages/Footer/Footer.jsx
@@ -1,32 +1,44 @@
-import { SiNextdotjs } from "react-icons/si";
+import { SiShopify } from "react-icons/si";
 import { Link } from "react-router";
 import { FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-horizontal footer-center bg-[#111827] text-primary-content p-10">
+      <footer className="footer footer-horizontal footer-center bg-base-200 text-headings p-10">
         <aside>
           <Link to="/">
-            <SiNextdotjs className="text-5xl" />
+            <SiShopify className="text-5xl" />
           </Link>
-          <p className="font-bold text-2xl">ACME Industries Ltd.</p>
-          <p className="font-semibold text-xl">
-            Providing reliable tech since 1992
+          <p className="font-bold text-2xl">Bazario Ltd.</p>
+          <p className="font-semibold text-xl text-descriptions">
+            Providing reliable tech since 2021
           </p>
-          <p className="text-[18px]">
+          <p className="text-base text-descriptions">
             Copyright © {new Date().getFullYear()} - All right reserved
           </p>
         </aside>
         <nav>
           <div className="grid grid-flow-col gap-4">
-            <Link to="https://x.com/" target="_blank">
+            <Link
+              to="https://x.com/"
+              target="_blank"
+              className="btn border-none shadow-none text-blue-500 bg-transparent"
+            >
               <FaTwitter className="text-3xl" />
             </Link>
-            <Link to="https://www.youtube.com/" target="_blank">
+            <Link
+              to="https://www.youtube.com/"
+              target="_blank"
+              className="btn border-none shadow-none text-red-500 bg-transparent"
+            >
               <FaYoutube className="text-3xl" />
             </Link>
-            <Link to="https://www.facebook.com/" target="_blank">
+            <Link
+              to="https://www.facebook.com/"
+              target="_blank"
+              className="btn border-none shadow-none text-blue-500 bg-transparent"
+            >
               <FaFacebookF className="text-3xl" />
             </Link>
           </div>

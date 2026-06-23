1ade14f6668ad104c51ffd190e3bb7b543cd84a4
saim-alam1 <allbehassansaim@gmail.com>
2026-06-23 15:18:57 +0600

    Loaded Vendors In Table And Added Suspend Vendors Feature

--- src/components/Pages/Admin/ManageVendors/ManageVendors.jsx
+++ src/components/Pages/Admin/ManageVendors/ManageVendors.jsx
@@ -1,7 +1,33 @@
+import { Helmet } from "react-helmet-async";
+import ManageVendorsTable from "./ManageVendorsTable";
+
 const ManageVendors = () => {
   return (
-    <section>
-      <h1>ManageVendors</h1>
+    <section className="my-14 px-3">
+      <Helmet>
+        <title>Manage Vendors | Admin Dashboard</title>
+        <meta
+          name="description"
+          content="Manage vendor accounts across the marketplace. Review vendor activity, verify sellers, suspend accounts that violate platform policies, reactivate approved vendors, or permanently remove accounts when necessary."
+        />
+      </Helmet>
+
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between pb-5 mb-8">
+        <div>
+          <h1 className="text-2xl font-bold tracking-tight text-headings sm:text-3xl">
+            Manage Vendor Accounts
+          </h1>
+
+          <p className="mt-1.5 text-sm text-descriptions">
+            Monitor and manage vendor accounts across the marketplace. Review
+            seller information, verify business legitimacy, identify policy
+            violations, suspend vendors involved in misconduct, reactivate
+            eligible accounts, or permanently remove vendors when necessary to
+            maintain a secure and trustworthy platform.
+          </p>
+        </div>
+      </div>
+      <ManageVendorsTable />
     </section>
   );
 };
---
+++ src/components/Pages/Admin/ManageVendors/ManageVendorsTable.jsx
@@ -0,0 +1,185 @@
+import LottiePlayer from "lottie-react";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import useAuth from "../../../../Hooks/useAuth";
+import useNotifications from "../../../../Hooks/useNotifications";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import Loading from "../../../UI/Loading/Loading";
+import noData from "../../../../assets/noData.json";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+
+const ManageVendorsTable = () => {
+  const Lottie = LottiePlayer.default || LottiePlayer;
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const { addNotification } = useNotifications();
+  const queryClient = useQueryClient();
+
+  const { data: vendors = [], isLoading } = useQuery({
+    queryKey: ["Load-vendors", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure("manage-vendors");
+      return res.data;
+    },
+  });
+
+  const handleVendorSuspend = (customerId, customerName, customerEmail) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `Suspend ${customerName}?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes, suspend it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        suspendVendor({
+          customerId,
+          customerName,
+          customerEmail,
+        });
+      }
+    });
+  };
+
+  const { mutate: suspendVendor, isPending: suspendingVendor } = useMutation({
+    mutationFn: async ({ customerId }) => {
+      const res = await axiosSecure.patch(`vendor/${customerId}/suspend`);
+      return res.data;
+    },
+    onSuccess: async (data, variable) => {
+      toast.success(
+        data?.message || `${variable.customerName} suspended successfully`,
+      );
+
+      queryClient.invalidateQueries({
+        queryKey: ["Load-vendors", user?.email],
+      });
+
+      // Posting Data In Notification Collection
+      addNotification({
+        receiverEmail: variable.customerEmail,
+        message: `Dear ${variable.customerName}, your vendor account on Bazario platform has been temporarily suspended!`,
+      });
+    },
+    onError: (error) => {
+      toast.error(
+        error.response?.data?.message || "An unexpected error occurred",
+      );
+    },
+  });
+
+  if (isLoading) return <Loading />;
+
+  if (!vendors?.length) {
+    return (
+      <div className="flex flex-col items-center justify-center py-10">
+        <Lottie animationData={noData} loop={true} className="w-72 md:w-96" />
+
+        <h3 className="text-3xl font-semibold text-headings">
+          No Customers Found
+        </h3>
+
+        <p className="text-descriptions mt-2">
+          There are currently no registered customers on the platform.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="overflow-x-auto bg-white rounded-xl border border-gray-100 shadow-sm">
+      <table className="table w-full">
+        <thead className="bg-gray-50">
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Contact No.</th>
+            <th>Status</th>
+            <th>Vendor Since</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {vendors.map((vendor) => (
+            <tr key={vendor._id} className="hover:bg-gray-50 transition">
+              <td>
+                <div className="flex items-center gap-3 whitespace-nowrap">
+                  <div className="text-headings">{vendor.fullName}</div>
+                </div>
+              </td>
+
+              <td className="text-headings">{vendor.email}</td>
+
+              <td className="text-headings">+{vendor.contactNumber}</td>
+
+              <td className="text-headings">
+                <span
+                  className={`font-medium ${
+                    vendor.status === "suspended"
+                      ? "text-red-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  {vendor.status === "suspended" ? "Suspended" : "Active"}
+                </span>
+              </td>
+
+              <td className="text-headings">
+                {new Date(vendor.approvedAt).toLocaleDateString("en-GB")}
+              </td>
+              <td>
+                {vendor.status === "suspended" ? (
+                  <button
+                    // disabled={reactivating}
+                    className="btn btn-success border-none shadow-none"
+                    // onClick={() =>
+                    //   handleReactivate(
+                    //     vendor._id,
+                    //     vendor.fullName,
+                    //     vendor.email,
+                    //   )
+                    // }
+                  >
+                    {/* {reactivating ? "Reactivating..." : "Reactivate"} */}
+                    Reactivate
+                  </button>
+                ) : (
+                  <button
+                    disabled={suspendingVendor}
+                    className="btn btn-warning border-none shadow-none"
+                    onClick={() =>
+                      handleVendorSuspend(
+                        vendor._id,
+                        vendor.fullName,
+                        vendor.email,
+                      )
+                    }
+                  >
+                    {suspendingVendor ? "Suspending..." : "Suspend"}
+                  </button>
+                )}
+              </td>
+
+              <td>
+                <button
+                  // disabled={removingCustomer}
+                  // onClick={() =>
+                  //   handleRemove(vendor._id, vendor.fullName, vendor.email)
+                  // }
+                  className="btn btn-error border-none shadow-none"
+                >
+                  {/* {removingCustomer ? "Removing..." : "Remove"} */} Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ManageVendorsTable;
